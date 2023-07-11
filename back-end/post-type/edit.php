f7b9803d0b789d69fdb2a268267182feabd523dc
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-07-11 14:42:59 +0200

    UPDATE ADD layout and email style

--- back-end/post-type/edit.php
+++ back-end/post-type/edit.php
@@ -14,40 +14,38 @@ function influactive_form_add_metaboxes(): void
 function influactive_form_metabox($post): void
 {
     ?>
+    <?php influactive_form_shortcode($post); ?>
     <div class="tabs">
         <ul class="tab-links">
-            <li class="active"><a href="#shortcode">Shortcode</a></li>
             <li><a href="#fields">Form Fields</a></li>
             <li><a href="#style">Form Style</a></li>
-            <li><a href="#email">Email Style</a></li>
+            <li class="active"><a href="#email">Email Layout</a></li>
         </ul>
 
         <div class="tab-content">
-            <div id="shortcode" class="tab active">
-                <!-- Shortcode content -->
-                <h2>Shortcode</h2>
-                <?php influactive_form_shortcode_metabox($post); ?>
-            </div>
             <div id="fields" class="tab">
                 <!-- Form fields content -->
                 <h2>Form Fields</h2>
                 <?php influactive_form_fields_listing($post); ?>
             </div>
             <div id="style" class="tab">
-                <!-- Form style content -->
-            </div>
-            <div id="email" class="tab">
                 <!-- Email style content -->
+                <h2>Form Style</h2>
+                <?php influactive_form_email_style($post); ?>
+            </div>
+            <div id="email" class="tab active">
+                <!-- Email style content -->
+                <h2>Email Layout</h2>
+                <?php influactive_form_email_layout($post); ?>
             </div>
         </div>
     </div>
     <?php
 }
 
-
-function influactive_form_shortcode_metabox($post): void
+function influactive_form_shortcode($post): void
 {
-    echo '[influactive_form id="' . $post->ID . '"]';
+    echo '<code>[influactive_form id="' . $post->ID . '"]</code>';
 }
 
 function influactive_form_fields_listing($post): void
@@ -67,7 +65,7 @@ function influactive_form_fields_listing($post): void
             echo '<option value="select" ' . (isset($field['type']) && $field['type'] === 'select' ? 'selected' : '') . '>Select</option>';
             echo '</select></label>';
             echo '<label>Label <input type="text" name="influactive_form_fields_label[' . $key . ']" value="' . esc_attr($field['label']) . '"></label> ';
-            echo '<label>Name <input type="text" name="influactive_form_fields_name[' . $key . ']" value="' . esc_attr($field['name']) . '"></label> ';
+            echo '<label>Name <input type="text" name="influactive_form_fields_name[' . $key . ']" value="' . strtolower(esc_attr($field['name'])) . '"></label> ';
             if (isset($field['type']) && $field['type'] === 'select') {
                 echo '<div class="options_container">';
                 if (is_array($field['options'])) {
@@ -89,6 +87,81 @@ function influactive_form_fields_listing($post): void
     echo '<p><a href="#" id="add_field">Add Field</a></p>';
 }
 
+function influactive_form_email_style($post): void
+{
+    $email_style = get_post_meta($post->ID, '_influactive_form_email_style', true);
+    ?>
+    <p>
+        <label>
+            Background color
+            <input type="color" name="influactive_form_email_style[background_color]"
+                   value="<?php echo esc_attr($email_style['background_color'] ?? ''); ?>">
+        </label>
+    </p>
+    <p>
+        <label>
+            Text color
+            <input type="color" name="influactive_form_email_style[text_color]"
+                   value="<?php echo esc_attr($email_style['text_color'] ?? ''); ?>">
+        </label>
+    </p>
+    <p>
+        <label>Link color
+            <input type="color" name="influactive_form_email_style[link_color]"
+                   value="<?php echo esc_attr($email_style['link_color'] ?? ''); ?>">
+        </label>
+    </p>
+    <p>
+        <label>
+            Link hover color
+            <input type="color" name="influactive_form_email_style[link_hover_color]"
+                   value="<?php echo esc_attr($email_style['link_hover_color'] ?? ''); ?>">
+        </label>
+    </p>
+    <?php
+}
+
+function influactive_form_email_layout($post): void
+{
+    $email_layout = get_post_meta($post->ID, '_influactive_form_email_layout', true);
+    ?>
+    <div id="influactive_form_layout_container">
+        <p>
+            <label>
+                Subject of the email
+                <input type="text" name="influactive_form_email_layout[subject]"
+                       value="<?= esc_attr($email_layout['subject'] ?? ''); ?>">
+            </label>
+        </p>
+        <p>
+            <label>
+                Content of the email
+                <textarea name="influactive_form_email_layout[content]" cols="30"
+                          rows="15"><?= esc_attr($email_layout['content'] ?? ''); ?></textarea>
+
+            </label>
+        </p>
+    </div>
+    <?php
+
+// List all influactive_form_fields_name like "{field_name}"
+    $fields = get_post_meta($post->ID, '_influactive_form_fields', true);
+    $fields_name = [];
+    if (is_array($fields)) {
+        foreach ($fields as $field) {
+            $fields_name[] = '{' . $field['name'] . '}';
+        }
+    }
+    ?>
+    <p><strong>Fields available in the email</strong></p>
+    <ul>
+        <?php foreach ($fields_name as $field_name): ?>
+            <li><code><?= strtolower($field_name) ?></code></li>
+        <?php endforeach; ?>
+    </ul>
+    <?php
+}
+
 // Enregistrement des champs
 add_action('save_post', 'influactive_form_save_post');
 function influactive_form_save_post($post_id): void
@@ -101,7 +174,7 @@ function influactive_form_save_post($post_id): void
         $field_order = $_POST['influactive_form_fields_order'] ?? [];
         $fields = [];
 
-        for ($i = 0, $iMax = count($fields_label); $i < $iMax; $i++) {
+        for ($i = 0, $iMax = count($fields_name); $i < $iMax; $i++) {
             $options = [];
             if (isset($fields_options[$field_order[$i]])) {
                 $options = is_array($fields_options[$field_order[$i]])
@@ -112,7 +185,7 @@ function influactive_form_save_post($post_id): void
             $fields[] = [
                 'type' => sanitize_text_field($fields_type[$i]),
                 'label' => sanitize_text_field($fields_label[$i]),
-                'name' => sanitize_text_field($fields_name[$i]),
+                'name' => strtolower(sanitize_text_field($fields_name[$i])),
                 'order' => (int)$field_order[$i],
             ];
 
@@ -122,5 +195,20 @@ function influactive_form_save_post($post_id): void
         }
 
         update_post_meta($post_id, '_influactive_form_fields', $fields);
+
+        $email_style = $_POST['influactive_form_email_style'] ?? [];
+        $email_layout = $_POST['influactive_form_email_layout'] ?? [];
+
+        // Sanitize email layout content
+        if (isset($email_layout['content'])) {
+            $email_layout['content'] = wp_kses_post($email_layout['content']);
+        }
+        // Sanitize email layout subject
+        if (isset($email_layout['subject'])) {
+            $email_layout['subject'] = sanitize_text_field($email_layout['subject']);
+        }
+
+        update_post_meta($post_id, '_influactive_form_email_style', $email_style);
+        update_post_meta($post_id, '_influactive_form_email_layout', $email_layout);
     }
 }
