7704455a788816a3ef4368daf66ee62165365523
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-07-11 14:43:29 +0200

    ADD influactive-form-layout style

--- functions.php
+++ functions.php
@@ -39,6 +39,8 @@ function influactive_form_edit($hook): void
     wp_enqueue_script('influactive-form-sortable', 'https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.15.0/Sortable.min.js', array(), '1.0', true);
     wp_enqueue_style('influactive-form', plugin_dir_url(__FILE__) . 'back-end/post-type/form.css', array(), '1.0');
 
-    wp_enqueue_script('influactive-tabs', plugin_dir_url(__FILE__) . 'back-end/post-type/tab.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('influactive-tabs', plugin_dir_url(__FILE__) . 'back-end/post-type/tab.js', array(), '1.0', true);
     wp_enqueue_style('influactive-tabs', plugin_dir_url(__FILE__) . 'back-end/post-type/tab.css', array(), '1.0');
+
+    wp_enqueue_style('influactive-form-layout', plugin_dir_url(__FILE__) . 'back-end/post-type/layout.css', array(), '1.0');
 }
