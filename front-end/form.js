ddf57a304ac720aeee7fdf6ced3f0e7405c13def
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-07-15 07:11:25 +0200

    ADD condition

--- front-end/form.js
+++ front-end/form.js
@@ -4,35 +4,37 @@ document.addEventListener('DOMContentLoaded', function() {
   const form = document.querySelector('.influactive-form');
   const messageDiv = document.querySelector('.influactive-form-message');
 
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
+  if (form) {
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
 
-    const xhr = new XMLHttpRequest();
-    const formData = new FormData(form);
-    formData.append('action', 'send_email');
+      const xhr = new XMLHttpRequest();
+      const formData = new FormData(form);
+      formData.append('action', 'send_email');
 
-    xhr.open('POST', ajax_object.ajaxurl, true);
+      xhr.open('POST', ajax_object.ajaxurl, true);
 
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        const response = JSON.parse(xhr.responseText);
-        if (response.data && response.data.sent) {
-          // Display the success message in the div
-          messageDiv.textContent = response.data.message;
-          form.reset();
-          setTimeout(function() {
-            messageDiv.textContent = '';
-          }, 2000);
+      xhr.onload = function () {
+        if (xhr.status === 200) {
+          const response = JSON.parse(xhr.responseText);
+          if (response.data && response.data.sent) {
+            // Display the success message in the div
+            messageDiv.textContent = response.data.message;
+            form.reset();
+            setTimeout(function () {
+              messageDiv.textContent = '';
+            }, 2000);
+          } else {
+            // Display the error message in the div
+            messageDiv.textContent = response.data.message;
+          }
         } else {
-          // Display the error message in the div
-          messageDiv.textContent = response.data.message;
+          // Display the AJAX error message in the div
+          messageDiv.textContent = 'Une erreur s\'est produite avec la requête AJAX';
         }
-      } else {
-        // Display the AJAX error message in the div
-        messageDiv.textContent = 'Une erreur s\'est produite avec la requête AJAX';
-      }
-    };
+      };
 
-    xhr.send(formData);
-  });
+      xhr.send(formData);
+    });
+  }
 });
