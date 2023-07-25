67c2c54fa5c3fe0c0f5357b43ef9071496b12dd8
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-07-25 20:54:17 +0200

    Change non-permission error message in edit.php
    Modified the function to return an exception instead of silently exiting when a user without 'edit_posts' capability tries to edit a post. This provides a clear error message, improving the UX.

--- back-end/post-type/edit.php
+++ back-end/post-type/edit.php
@@ -24,7 +24,7 @@ add_action('add_meta_boxes', 'influactive_form_add_metaboxes');
 function influactive_form_metabox(WP_Post $post): void
 {
     if (!current_user_can('edit_posts')) {
-        return;
+        throw new RuntimeException("WordPress environment not loaded. Exiting...");
     }
 
     ?>
