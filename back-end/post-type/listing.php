6e34811044aff7b8a312863630246341201d5d18
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-07-25 09:28:47 +0200

    Enhance code readability and error message in Influactive Forms plugin
    These changes provide improvements to the Influactive Forms WordPress plugin. Firstly, a more descriptive error message is thrown if WordPress environment is not loaded, enhancing debugging. Secondly, PHPDocs are added to functions 'influactive_form_posts_columns' and 'influactive_form_posts_custom_column' for better clarity. Lastly, function signatures are explicitly typed to ensure correct data types are passed to enhance code reliability.

--- back-end/post-type/listing.php
+++ back-end/post-type/listing.php
@@ -1,22 +1,36 @@
 <?php
 
 if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly.
+    throw new RuntimeException("WordPress environment not loaded. Exiting...");
 }
 
 // Ajouter la colonne dans la page de la liste des posts
 add_filter('manage_influactive-forms_posts_columns', 'influactive_form_posts_columns');
 
-function influactive_form_posts_columns($columns): array
+/**
+ * Modifies the columns for the Influactive Form post-type.
+ *
+ * @param array $columns An array of the current columns.
+ *
+ * @return array Modified array of columns with 'shortcode' column added.
+ */
+function influactive_form_posts_columns(array $columns): array
 {
     $columns['shortcode'] = 'Shortcode';
-
     return $columns;
 }
 
 add_action('manage_influactive-forms_posts_custom_column', 'influactive_form_posts_custom_column', 10, 2);
 
-function influactive_form_posts_custom_column($column, $post_id): void
+/**
+ * Displays the custom column content for the Influactive Form post-type.
+ *
+ * @param string $column The name of the custom column.
+ * @param int $post_id The ID of the current post.
+ *
+ * @return void
+ */
+function influactive_form_posts_custom_column(string $column, int $post_id): void
 {
     if ($column === 'shortcode') {
         echo '[influactive_form id="' . $post_id . '"]';
