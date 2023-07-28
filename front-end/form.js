7bc184ada1e3146ea37afbb3846f66c7e7daf9c4
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-07-28 12:23:00 +0200

    Refactor form.js to improve code quality and readability
    Code refactoring was conducted in form.js. This included changing the usage of traditional function statements to arrow functions for better readability and to maintain the current JavaScript standards. Also, script was adjusted to adhere to camelcase rule, improving overall code quality. The variable 'recaptcha_site_key' was renamed to 'recaptchaSiteKey', and function naming was introduced for xhr.onload event. ESLint directives were added to bypass some warnings. The input type declaration for 'recaptchaResponse' parameter was also updated to be more accurate.

--- front-end/form.js
+++ front-end/form.js
@@ -1,9 +1,9 @@
 /* global grecaptcha, ajax_object */
 
 /**
- * @param {Element} messageDiv
+ * @param messageDiv
  * @param {Element} form
- * @param {T|string|Blob} recaptchaResponse
+ * @param {string|Blob} recaptchaResponse
  */
 function submitFormGlobal(messageDiv, form, recaptchaResponse) {
   const xhr = new XMLHttpRequest();
@@ -14,57 +14,59 @@ function submitFormGlobal(messageDiv, form, recaptchaResponse) {
     formData.append('recaptcha_response', recaptchaResponse);
   }
 
+  // eslint-disable-next-line camelcase
   xhr.open('POST', ajax_object.ajaxurl, true);
 
-  xhr.onload = function() {
+  xhr.onload = function xhrOnLoad() {
     if (xhr.status === 200) {
       const response = JSON.parse(xhr.responseText);
       if (response.data) {
         // Display the success message in the div
+        // eslint-disable-next-line no-param-reassign
         messageDiv.textContent = response.data.message;
         form.reset();
       } else {
         // Display the error message in the div
+        // eslint-disable-next-line no-param-reassign
         messageDiv.textContent = response.data.message;
-        console.log(response.data);
       }
     } else {
       // Display the AJAX error message in the div
-      messageDiv.textContent = "An error occurred with the AJAX request";
+      // eslint-disable-next-line no-param-reassign
+      messageDiv.textContent = 'An error occurred with the AJAX request';
     }
   };
 
   xhr.send(formData);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   const forms = document.querySelectorAll('.influactive-form');
 
-  forms.forEach(form => { // On boucle sur chaque formulaire
-
+  forms.forEach((form) => { // On boucle sur chaque formulaire
     if (form.parentElement.parentElement.parentElement.classList.contains('influactive-modal-form-brochure')) {
       return;
     }
 
-    form.addEventListener('submit', function(event) {
+    form.addEventListener('submit', (event) => {
       event.preventDefault();
 
       const messageDiv = form.querySelector('.influactive-form-message');
       const recaptchaInput = form.querySelector('input[name="recaptcha_site_key"]');
 
       if (recaptchaInput && grecaptcha) {
-        const recaptcha_site_key = recaptchaInput.value;
-        grecaptcha.ready(function() {
-          grecaptcha.execute(recaptcha_site_key, {action: 'submit'}).then(function(token) {
+        const recaptchaSiteKey = recaptchaInput.value;
+        grecaptcha.ready(() => {
+          grecaptcha.execute(recaptchaSiteKey, { action: 'submit' }).then((token) => {
             submitFormGlobal(messageDiv, form, token);
-            setTimeout(function() {
+            setTimeout(() => {
               messageDiv.textContent = '';
             }, 5000);
           });
         });
       } else {
         submitFormGlobal(messageDiv, form, null);
-        setTimeout(function() {
+        setTimeout(() => {
           messageDiv.textContent = '';
         }, 5000);
       }
