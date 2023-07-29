5d89f1c89716cebdaceef4af9889064b664ee096
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-07-29 20:38:11 +0200

    Update styling variable sanitisation in dynamic-style.php
    Enhanced the security of the dynamic-style.php file on the 'Influactive Forms' plugin by applying the esc_html function to all styling variables. This is necessary to prevent potential Cross-Site Scripting (XSS) issues, where an attacker could inject malicious code via these styling variables. Now all values passed would be properly escaped, ensuring they are safe for displaying to users.

--- front-end/dynamic-style.php
+++ front-end/dynamic-style.php
@@ -29,19 +29,19 @@ $free_text   = $email_style['free_text'];
 ob_start();
 ?>
 	.influactive-form-wrapper {
-	padding: <?php echo $form['padding']; ?>;
-	background-color: <?php echo $form['background_color']; ?>;
-	border-width: <?php echo $form['border_width']; ?>;
-	border-style: <?php echo $form['border_style']; ?>;
-	border-color: <?php echo $form['border_color']; ?>;
+	padding: <?php echo esc_html( $form['padding'] ); ?>;
+	background-color: <?php echo esc_html( $form['background_color'] ); ?>;
+	border-width: <?php echo esc_html( $form['border_width'] ); ?>;
+	border-style: <?php echo esc_html( $form['border_style'] ); ?>;
+	border-color: <?php echo esc_html( $form['border_color'] ); ?>;
 	}
 
 	.influactive-form-wrapper label {
-	font-weight: <?php echo $label['font_weight']; ?>;
-	font-family: <?php echo $label['font_family']; ?>;
-	font-size: <?php echo $label['font_size']; ?>;
-	color: <?php echo $label['font_color']; ?>;
-	line-height: <?php echo $label['line_height']; ?>;
+	font-weight: <?php echo esc_html( $label['font_weight'] ); ?>;
+	font-family: <?php echo esc_html( $label['font_family'] ); ?>;
+	font-size: <?php echo esc_html( $label['font_size'] ); ?>;
+	color: <?php echo esc_html( $label['font_color'] ); ?>;
+	line-height: <?php echo esc_html( $label['line_height'] ); ?>;
 	}
 
 	.influactive-form-wrapper input[type="text"],
@@ -49,52 +49,52 @@ ob_start();
 	.influactive-form-wrapper input[type="number"],
 	.influactive-form-wrapper textarea,
 	.influactive-form-wrapper select {
-	padding: <?php echo $input['padding']; ?>;
-	border-width: <?php echo $input['border_width']; ?>;
-	border-style: <?php echo $input['border_style']; ?>;
-	border-color: <?php echo $input['border_color']; ?>;
-	border-radius: <?php echo $input['border_radius']; ?>;
-	background-color: <?php echo $input['background_color']; ?>;
-	color: <?php echo $input['font_color']; ?>;
-	font-size: <?php echo $input['font_size']; ?>;
-	font-weight: <?php echo $input['font_weight']; ?>;
-	font-family: <?php echo $input['font_family']; ?>;
-	line-height: <?php echo $input['line_height']; ?>;
+	padding: <?php echo esc_html( $input['padding'] ); ?>;
+	border-width: <?php echo esc_html( $input['border_width'] ); ?>;
+	border-style: <?php echo esc_html( $input['border_style'] ); ?>;
+	border-color: <?php echo esc_html( $input['border_color'] ); ?>;
+	border-radius: <?php echo esc_html( $input['border_radius'] ); ?>;
+	background-color: <?php echo esc_html( $input['background_color'] ); ?>;
+	color: <?php echo esc_html( $input['font_color'] ); ?>;
+	font-size: <?php echo esc_html( $input['font_size'] ); ?>;
+	font-weight: <?php echo esc_html( $input['font_weight'] ); ?>;
+	font-family: <?php echo esc_html( $input['font_family'] ); ?>;
+	line-height: <?php echo esc_html( $input['line_height'] ); ?>;
 	}
 
 	.influactive-form-wrapper input[type="checkbox"] {
-	color: <?php echo $input['font_color']; ?>;
-	font-size: <?php echo $input['font_size']; ?>;
-	font-weight: <?php echo $input['font_weight']; ?>;
-	font-family: <?php echo $input['font_family']; ?>;
-	line-height: <?php echo $input['line_height']; ?>;
+	color: <?php echo esc_html( $input['font_color'] ); ?>;
+	font-size: <?php echo esc_html( $input['font_size'] ); ?>;
+	font-weight: <?php echo esc_html( $input['font_weight'] ); ?>;
+	font-family: <?php echo esc_html( $input['font_family'] ); ?>;
+	line-height: <?php echo esc_html( $input['line_height'] ); ?>;
 	}
 
 	.influactive-form-wrapper input[type="submit"] {
-	padding: <?php echo $submit['padding']; ?>;
-	color: <?php echo $submit['font_color']; ?>;
-	background-color: <?php echo $submit['background_color']; ?>;
-	border-radius: <?php echo $submit['border_radius']; ?>;
-	border-width: <?php echo $submit['border_width']; ?>;
-	border-style: <?php echo $submit['border_style']; ?>;
-	border-color: <?php echo $submit['border_color']; ?>;
-	font-size: <?php echo $submit['font_size']; ?>;
-	font-weight: <?php echo $submit['font_weight']; ?>;
-	font-family: <?php echo $submit['font_family']; ?>;
-	line-height: <?php echo $submit['line_height']; ?>;
+	padding: <?php echo esc_html( $submit['padding'] ); ?>;
+	color: <?php echo esc_html( $submit['font_color'] ); ?>;
+	background-color: <?php echo esc_html( $submit['background_color'] ); ?>;
+	border-radius: <?php echo esc_html( $submit['border_radius'] ); ?>;
+	border-width: <?php echo esc_html( $submit['border_width'] ); ?>;
+	border-style: <?php echo esc_html( $submit['border_style'] ); ?>;
+	border-color: <?php echo esc_html( $submit['border_color'] ); ?>;
+	font-size: <?php echo esc_html( $submit['font_size'] ); ?>;
+	font-weight: <?php echo esc_html( $submit['font_weight'] ); ?>;
+	font-family: <?php echo esc_html( $submit['font_family'] ); ?>;
+	line-height: <?php echo esc_html( $submit['line_height'] ); ?>;
 	}
 
 	.influactive-form-wrapper input[type="submit"]:hover {
-	background-color: <?php echo $submit['background_hover_color']; ?>;
-	color: <?php echo $submit['font_hover_color']; ?>;
+	background-color: <?php echo esc_html( $submit['background_hover_color'] ); ?>;
+	color: <?php echo esc_html( $submit['font_hover_color'] ); ?>;
 	}
 
 	.influactive-form-wrapper .free-text {
-	font-size: <?php echo $free_text['font_size']; ?>;
-	font-weight: <?php echo $free_text['font_weight']; ?>;
-	font-family: <?php echo $free_text['font_family']; ?>;
-	line-height: <?php echo $free_text['line_height']; ?>;
-	color: <?php echo $free_text['color']; ?>;
+	font-size: <?php echo esc_html( $free_text['font_size'] ); ?>;
+	font-weight: <?php echo esc_html( $free_text['font_weight'] ); ?>;
+	font-family: <?php echo esc_html( $free_text['font_family'] ); ?>;
+	line-height: <?php echo esc_html( $free_text['line_height'] ); ?>;
+	color: <?php echo esc_html( $free_text['color'] ); ?>;
 	}
 
 <?php
