3abeef0e668bca62fbb0b12710395d4432186fdc
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-07-29 20:51:56 +0200

    Refactor shortcode generation for security
    Refactored the shortcode generation in the Influactive Forms plugin to use the WordPress function esc_attr(). This change ensures that the post_id is properly escaped to prevent possible security vulnerabilities from unescaped user inputs. Additionally, the condition syntax for checking the 'shortcode' column is updated for consistency.

--- back-end/post-type/listing.php
+++ back-end/post-type/listing.php
@@ -27,16 +27,16 @@ function influactive_form_posts_columns( array $columns ): array {
 add_filter( 'manage_influactive-forms_posts_columns', 'influactive_form_posts_columns' );
 
 /**
- * Displays the custom column content for the Influactive Form post-type.
+ * Generates a custom column value for the "influactive_form" post type.
  *
- * @param string $column The name of the custom column.
- * @param int $post_id The ID of the current post.
+ * @param string $column The name of the column.
+ * @param int $post_id The ID of the post.
  *
  * @return void
  */
 function influactive_form_posts_custom_column( string $column, int $post_id ): void {
-	if ( $column === 'shortcode' ) {
-		echo '[influactive_form id="' . $post_id . '"]';
+	if ( 'shortcode' === $column ) {
+		echo '[influactive_form id="' . esc_attr( $post_id ) . '"]';
 	}
 }
 
