40e383cf5030b9a1dc75360b2a918eedfb271238
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-07-29 20:30:35 +0200

    Refactor dynamic-style.php, improve code consistency and readability
    Changed shorthand PHP open tags into full open tags (<? to <?php) for better compatibility and increased code readability. Renamed variable $post_id to $my_post_id to avoid possible collisions. Final output remains the same, using standard php open tags which enhances the maintainability and readability of the code.

--- front-end/dynamic-style.php
+++ front-end/dynamic-style.php
@@ -9,17 +9,17 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	requireWordPressCore(
 		array(
-			'/wp-load.php', // WordPress
-			'/wordpress/wp-load.php', // WordPlate
-			'/wp/wp-load.php', // Radicle
+			'/wp-load.php', // WordPress.
+			'/wordpress/wp-load.php', // WordPlate.
+			'/wp/wp-load.php', // Radicle.
 		)
 	);
 }
 
 header( 'Content-type: text/css; charset: UTF-8' );
 
-$post_id     = isset( $_GET['post_id'] ) ? (int) $_GET['post_id'] : 0;
-$email_style = get_post_meta( $post_id, '_influactive_form_email_style', true );
+$my_post_id  = isset( $_GET['post_id'] ) ? (int) $_GET['post_id'] : 0;
+$email_style = get_post_meta( $my_post_id, '_influactive_form_email_style', true );
 $form        = $email_style['form'];
 $label       = $email_style['label'];
 $input       = $email_style['input'];
@@ -29,19 +29,19 @@ $free_text   = $email_style['free_text'];
 ob_start();
 ?>
 	.influactive-form-wrapper {
-	padding: <?= $form['padding'] ?>;
-	background-color: <?= $form['background_color'] ?>;
-	border-width: <?= $form['border_width'] ?>;
-	border-style: <?= $form['border_style'] ?>;
-	border-color: <?= $form['border_color'] ?>;
+	padding: <?php echo $form['padding']; ?>;
+	background-color: <?php echo $form['background_color']; ?>;
+	border-width: <?php echo $form['border_width']; ?>;
+	border-style: <?php echo $form['border_style']; ?>;
+	border-color: <?php echo $form['border_color']; ?>;
 	}
 
 	.influactive-form-wrapper label {
-	font-weight: <?= $label['font_weight'] ?>;
-	font-family: <?= $label['font_family'] ?>;
-	font-size: <?= $label['font_size'] ?>;
-	color: <?= $label['font_color'] ?>;
-	line-height: <?= $label['line_height'] ?>;
+	font-weight: <?php echo $label['font_weight']; ?>;
+	font-family: <?php echo $label['font_family']; ?>;
+	font-size: <?php echo $label['font_size']; ?>;
+	color: <?php echo $label['font_color']; ?>;
+	line-height: <?php echo $label['line_height']; ?>;
 	}
 
 	.influactive-form-wrapper input[type="text"],
@@ -49,52 +49,52 @@ ob_start();
 	.influactive-form-wrapper input[type="number"],
 	.influactive-form-wrapper textarea,
 	.influactive-form-wrapper select {
-	padding: <?= $input['padding'] ?>;
-	border-width: <?= $input['border_width'] ?>;
-	border-style: <?= $input['border_style'] ?>;
-	border-color: <?= $input['border_color'] ?>;
-	border-radius: <?= $input['border_radius'] ?>;
-	background-color: <?= $input['background_color'] ?>;
-	color: <?= $input['font_color'] ?>;
-	font-size: <?= $input['font_size'] ?>;
-	font-weight: <?= $input['font_weight'] ?>;
-	font-family: <?= $input['font_family'] ?>;
-	line-height: <?= $input['line_height'] ?>;
+	padding: <?php echo $input['padding']; ?>;
+	border-width: <?php echo $input['border_width']; ?>;
+	border-style: <?php echo $input['border_style']; ?>;
+	border-color: <?php echo $input['border_color']; ?>;
+	border-radius: <?php echo $input['border_radius']; ?>;
+	background-color: <?php echo $input['background_color']; ?>;
+	color: <?php echo $input['font_color']; ?>;
+	font-size: <?php echo $input['font_size']; ?>;
+	font-weight: <?php echo $input['font_weight']; ?>;
+	font-family: <?php echo $input['font_family']; ?>;
+	line-height: <?php echo $input['line_height']; ?>;
 	}
 
 	.influactive-form-wrapper input[type="checkbox"] {
-	color: <?= $input['font_color'] ?>;
-	font-size: <?= $input['font_size'] ?>;
-	font-weight: <?= $input['font_weight'] ?>;
-	font-family: <?= $input['font_family'] ?>;
-	line-height: <?= $input['line_height'] ?>;
+	color: <?php echo $input['font_color']; ?>;
+	font-size: <?php echo $input['font_size']; ?>;
+	font-weight: <?php echo $input['font_weight']; ?>;
+	font-family: <?php echo $input['font_family']; ?>;
+	line-height: <?php echo $input['line_height']; ?>;
 	}
 
 	.influactive-form-wrapper input[type="submit"] {
-	padding: <?= $submit['padding'] ?>;
-	color: <?= $submit['font_color'] ?>;
-	background-color: <?= $submit['background_color'] ?>;
-	border-radius: <?= $submit['border_radius'] ?>;
-	border-width: <?= $submit['border_width'] ?>;
-	border-style: <?= $submit['border_style'] ?>;
-	border-color: <?= $submit['border_color'] ?>;
-	font-size: <?= $submit['font_size'] ?>;
-	font-weight: <?= $submit['font_weight'] ?>;
-	font-family: <?= $submit['font_family'] ?>;
-	line-height: <?= $submit['line_height'] ?>;
+	padding: <?php echo $submit['padding']; ?>;
+	color: <?php echo $submit['font_color']; ?>;
+	background-color: <?php echo $submit['background_color']; ?>;
+	border-radius: <?php echo $submit['border_radius']; ?>;
+	border-width: <?php echo $submit['border_width']; ?>;
+	border-style: <?php echo $submit['border_style']; ?>;
+	border-color: <?php echo $submit['border_color']; ?>;
+	font-size: <?php echo $submit['font_size']; ?>;
+	font-weight: <?php echo $submit['font_weight']; ?>;
+	font-family: <?php echo $submit['font_family']; ?>;
+	line-height: <?php echo $submit['line_height']; ?>;
 	}
 
 	.influactive-form-wrapper input[type="submit"]:hover {
-	background-color: <?= $submit['background_hover_color'] ?>;
-	color: <?= $submit['font_hover_color'] ?>;
+	background-color: <?php echo $submit['background_hover_color']; ?>;
+	color: <?php echo $submit['font_hover_color']; ?>;
 	}
 
 	.influactive-form-wrapper .free-text {
-	font-size: <?= $free_text['font_size'] ?>;
-	font-weight: <?= $free_text['font_weight'] ?>;
-	font-family: <?= $free_text['font_family'] ?>;
-	line-height: <?= $free_text['line_height'] ?>;
-	color: <?= $free_text['color'] ?>;
+	font-size: <?php echo $free_text['font_size']; ?>;
+	font-weight: <?php echo $free_text['font_weight']; ?>;
+	font-family: <?php echo $free_text['font_family']; ?>;
+	line-height: <?php echo $free_text['line_height']; ?>;
+	color: <?php echo $free_text['color']; ?>;
 	}
 
 <?php
