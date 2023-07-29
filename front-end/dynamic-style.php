47a329e6e983e19fb7ffa5754763403df85cdc49
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-07-29 20:22:53 +0200

    Refactor spaces for readability in dynamic-style.php
    This commit modifies the dynamic-style.php file within the Influactive Forms wordpress plugin. Changes pertain to altering margins and indents to space codes for readability and for keeping conformance with code style guidelines. The indentation has been changed from 4 spaces per tab to a single tab in order to provide better visual separation for nested elements. This update improves legibility making code review and debugging more manageable.

--- front-end/dynamic-style.php
+++ front-end/dynamic-style.php
@@ -7,11 +7,11 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    requireWordPressCore( [
-            '/wp-load.php', // WordPress
-            '/wordpress/wp-load.php', // WordPlate
-            '/wp/wp-load.php', // Radicle
-    ] );
+	requireWordPressCore( [
+		'/wp-load.php', // WordPress
+		'/wordpress/wp-load.php', // WordPlate
+		'/wp/wp-load.php', // Radicle
+	] );
 }
 
 header( 'Content-type: text/css; charset: UTF-8' );
@@ -26,74 +26,74 @@ $free_text   = $email_style['free_text'];
 
 ob_start();
 ?>
-    .influactive-form-wrapper {
-    padding: <?= $form['padding'] ?>;
-    background-color: <?= $form['background_color'] ?>;
-    border-width: <?= $form['border_width'] ?>;
-    border-style: <?= $form['border_style'] ?>;
-    border-color: <?= $form['border_color'] ?>;
-    }
+	.influactive-form-wrapper {
+	padding: <?= $form['padding'] ?>;
+	background-color: <?= $form['background_color'] ?>;
+	border-width: <?= $form['border_width'] ?>;
+	border-style: <?= $form['border_style'] ?>;
+	border-color: <?= $form['border_color'] ?>;
+	}
 
-    .influactive-form-wrapper label {
-    font-weight: <?= $label['font_weight'] ?>;
-    font-family: <?= $label['font_family'] ?>;
-    font-size: <?= $label['font_size'] ?>;
-    color: <?= $label['font_color'] ?>;
-    line-height: <?= $label['line_height'] ?>;
-    }
+	.influactive-form-wrapper label {
+	font-weight: <?= $label['font_weight'] ?>;
+	font-family: <?= $label['font_family'] ?>;
+	font-size: <?= $label['font_size'] ?>;
+	color: <?= $label['font_color'] ?>;
+	line-height: <?= $label['line_height'] ?>;
+	}
 
-    .influactive-form-wrapper input[type="text"],
-    .influactive-form-wrapper input[type="email"],
-    .influactive-form-wrapper input[type="number"],
-    .influactive-form-wrapper textarea,
-    .influactive-form-wrapper select {
-    padding: <?= $input['padding'] ?>;
-    border-width: <?= $input['border_width'] ?>;
-    border-style: <?= $input['border_style'] ?>;
-    border-color: <?= $input['border_color'] ?>;
-    border-radius: <?= $input['border_radius'] ?>;
-    background-color: <?= $input['background_color'] ?>;
-    color: <?= $input['font_color'] ?>;
-    font-size: <?= $input['font_size'] ?>;
-    font-weight: <?= $input['font_weight'] ?>;
-    font-family: <?= $input['font_family'] ?>;
-    line-height: <?= $input['line_height'] ?>;
-    }
+	.influactive-form-wrapper input[type="text"],
+	.influactive-form-wrapper input[type="email"],
+	.influactive-form-wrapper input[type="number"],
+	.influactive-form-wrapper textarea,
+	.influactive-form-wrapper select {
+	padding: <?= $input['padding'] ?>;
+	border-width: <?= $input['border_width'] ?>;
+	border-style: <?= $input['border_style'] ?>;
+	border-color: <?= $input['border_color'] ?>;
+	border-radius: <?= $input['border_radius'] ?>;
+	background-color: <?= $input['background_color'] ?>;
+	color: <?= $input['font_color'] ?>;
+	font-size: <?= $input['font_size'] ?>;
+	font-weight: <?= $input['font_weight'] ?>;
+	font-family: <?= $input['font_family'] ?>;
+	line-height: <?= $input['line_height'] ?>;
+	}
 
-    .influactive-form-wrapper input[type="checkbox"] {
-    color: <?= $input['font_color'] ?>;
-    font-size: <?= $input['font_size'] ?>;
-    font-weight: <?= $input['font_weight'] ?>;
-    font-family: <?= $input['font_family'] ?>;
-    line-height: <?= $input['line_height'] ?>;
-    }
+	.influactive-form-wrapper input[type="checkbox"] {
+	color: <?= $input['font_color'] ?>;
+	font-size: <?= $input['font_size'] ?>;
+	font-weight: <?= $input['font_weight'] ?>;
+	font-family: <?= $input['font_family'] ?>;
+	line-height: <?= $input['line_height'] ?>;
+	}
 
-    .influactive-form-wrapper input[type="submit"] {
-    padding: <?= $submit['padding'] ?>;
-    color: <?= $submit['font_color'] ?>;
-    background-color: <?= $submit['background_color'] ?>;
-    border-radius: <?= $submit['border_radius'] ?>;
-    border-width: <?= $submit['border_width'] ?>;
-    border-style: <?= $submit['border_style'] ?>;
-    border-color: <?= $submit['border_color'] ?>;
-    font-size: <?= $submit['font_size'] ?>;
-    font-weight: <?= $submit['font_weight'] ?>;
-    font-family: <?= $submit['font_family'] ?>;
-    line-height: <?= $submit['line_height'] ?>;
-    }
+	.influactive-form-wrapper input[type="submit"] {
+	padding: <?= $submit['padding'] ?>;
+	color: <?= $submit['font_color'] ?>;
+	background-color: <?= $submit['background_color'] ?>;
+	border-radius: <?= $submit['border_radius'] ?>;
+	border-width: <?= $submit['border_width'] ?>;
+	border-style: <?= $submit['border_style'] ?>;
+	border-color: <?= $submit['border_color'] ?>;
+	font-size: <?= $submit['font_size'] ?>;
+	font-weight: <?= $submit['font_weight'] ?>;
+	font-family: <?= $submit['font_family'] ?>;
+	line-height: <?= $submit['line_height'] ?>;
+	}
 
-    .influactive-form-wrapper input[type="submit"]:hover {
-    background-color: <?= $submit['background_hover_color'] ?>;
-    color: <?= $submit['font_hover_color'] ?>;
-    }
+	.influactive-form-wrapper input[type="submit"]:hover {
+	background-color: <?= $submit['background_hover_color'] ?>;
+	color: <?= $submit['font_hover_color'] ?>;
+	}
 
-    .influactive-form-wrapper .free-text {
-    font-size: <?= $free_text['font_size'] ?>;
-    font-weight: <?= $free_text['font_weight'] ?>;
-    font-family: <?= $free_text['font_family'] ?>;
-    line-height: <?= $free_text['line_height'] ?>;
-    color: <?= $free_text['color'] ?>;
-    }
+	.influactive-form-wrapper .free-text {
+	font-size: <?= $free_text['font_size'] ?>;
+	font-weight: <?= $free_text['font_weight'] ?>;
+	font-family: <?= $free_text['font_family'] ?>;
+	line-height: <?= $free_text['line_height'] ?>;
+	color: <?= $free_text['color'] ?>;
+	}
 
 <?php
 $css = ob_get_clean();
