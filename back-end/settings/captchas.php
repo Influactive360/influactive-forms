37002ad109dfa424010e1ff0fb2eabcae5d7bed8
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-08-03 16:12:19 +0200

    Refactor captcha site key prompt in `captchas.php`
    Switched `wp_kses` to `wp_kses_post` to simplify the code and uplift security in `captchas.php`. Removed specific whitelist HTML 'a' attributes as `wp_kses_post` already covers all allowable HTML attributes. This change makes the code neater, easier to maintain, and reduces the risk of potential security exploits by restricting the use of HTML in output.

--- back-end/settings/captchas.php
+++ back-end/settings/captchas.php
@@ -120,16 +120,9 @@ function influactive_forms_settings_section_callback(): void {
 	);
 
 	printf(
-		wp_kses(
+		wp_kses_post(
 		/* translators: %s: URL of the Google captcha site key generation page */
-			__( 'Enter the Google Captcha site key. Here is the link to generate a key: %s', 'influactive-forms' ),
-			array(
-				'a' => array(
-					'href'   => array(),
-					'target' => array(),
-					'title'  => array(),
-				),
-			)
+			__( 'Enter the Google Captcha site key. Here is the link to generate a key: %s', 'influactive-forms' )
 		),
 		esc_attr( $recaptcha_link )
 	);
